3ac864d152bfb834a57afd50d3fbf8a7550d764e
Karol P <kpawlx@gmail.com>
2023-07-10 21:48:59 +0200

    Szkielet strony

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import '$lib/styles/style.css';
+</script>
+
+<header>
+	<a href="/">Home</a>
+	<nav>
+		<ul>
+			<li><a href="/blog">Blog</a></li>
+			<li><a href="/portfolio">Portfolio</a></li>
+			<li><a href="/about">O mnie</a></li>
+			<li><a href="/contact">Kontakt</a></li>
+		</ul>
+	</nav>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>&copy; 2023 kpawlx</footer>
